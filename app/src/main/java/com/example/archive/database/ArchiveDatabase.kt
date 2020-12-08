de73272852199bba7b6568fbd5dcd88fd5c1e79e
KonstantinShr <sharlaev2000@yandex.ru>
2020-12-08 23:36:05 +0300

    Added all dao's and getInstance method

--- app/src/main/java/com/example/archive/database/ArchiveDatabase.kt
+++ app/src/main/java/com/example/archive/database/ArchiveDatabase.kt
@@ -1,6 +1,56 @@
 package com.example.archive.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.archive.database.dao.*
+import com.example.archive.database.enteties.*
 
+@Database(entities = [Cell::class,
+                      Department::class,
+                      Document::class,
+                      DocumentTheme::class,
+                      User::class], version = 1, exportSchema = false)
 abstract class ArchiveDatabase : RoomDatabase() {
+
+    abstract val cellDao: CellDao
+
+    abstract val departmentDao: DepartmentDao
+
+    abstract val documentDao: DocumentDao
+
+    abstract val documentThemeDao: DocumentThemeDao
+
+    abstract val userDao: UserDao
+
+    companion object{
+        /**
+         * INSTANCE will keep a reference to any database returned via getInstance.
+         *
+         * This will help us avoid repeatedly initializing the database, which is expensive.
+         *
+         *  The value of a volatile variable will never be cached, and all writes and
+         *  reads will be done to and from the main memory. It means that changes made by one
+         *  thread to shared data are visible to other threads.
+         */
+        @Volatile
+        private var INSTANCE: ArchiveDatabase? = null
+
+        fun getInstance(context: Context) : ArchiveDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                                                    ArchiveDatabase::class.java,
+                                                    "archive_database")
+                                    .fallbackToDestructiveMigration()
+                                    .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
