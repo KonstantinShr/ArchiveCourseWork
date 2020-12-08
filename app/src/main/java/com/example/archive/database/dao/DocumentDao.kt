dfce004fe098e175cc7f77c8bf2e634897febab3
KonstantinShr <sharlaev2000@yandex.ru>
2020-12-08 23:13:02 +0300

    Creation document dao

---
+++ app/src/main/java/com/example/archive/database/dao/DocumentDao.kt
@@ -0,0 +1,20 @@
+package com.example.archive.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import com.example.archive.database.enteties.Department
+import com.example.archive.database.enteties.Document
+
+@Dao
+interface DocumentDao {
+    @Insert
+    suspend fun insert(document: Document)
+
+    @Update
+    suspend fun update(document: Document)
+
+    @Delete
+    suspend fun delete(document: Document)
+}
