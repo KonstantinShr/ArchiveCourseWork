42f6a00226e968b9b720bfa90685da2adecf644f
KonstantinShr <sharlaev2000@yandex.ru>
2020-12-08 23:20:09 +0300

    .

--- app/src/main/java/com/example/archive/database/dao/DocumentDao.kt
+++ app/src/main/java/com/example/archive/database/dao/DocumentDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Update
-import com.example.archive.database.enteties.Department
 import com.example.archive.database.enteties.Document
 
 @Dao
--- app/src/main/java/com/example/archive/database/dao/DocumentThemeDao.kt
+++ app/src/main/java/com/example/archive/database/dao/DocumentThemeDao.kt
@@ -1,10 +1,12 @@
 package com.example.archive.database.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Update
 import com.example.archive.database.enteties.DocumentTheme
 
+@Dao
 interface DocumentThemeDao {
     @Insert
     suspend fun insert(documentTheme: DocumentTheme)
