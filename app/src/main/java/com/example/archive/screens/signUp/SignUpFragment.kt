9cef8ccd02a28c49a7cae983e2d3ba7bc5d043e5
KonstantinShr <sharlaev2000@yandex.ru>
2020-12-11 15:47:06 +0300

    .

--- app/src/main/java/com/example/archive/screens/signUp/SignUpFragment.kt
+++ app/src/main/java/com/example/archive/screens/signUp/SignUpFragment.kt
@@ -6,26 +6,66 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.archive.R
-import com.example.archive.screens.signUp.SignUpFragmentDirections
+import com.example.archive.database.ArchiveDatabase
 import com.example.archive.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var viewModel: SignUpViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentSignUpBinding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_sign_up,
-            container,
-            false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up, container, false)
 
-        binding.signUpBtn.setOnClickListener { view: View ->
-            view.findNavController().navigate(SignUpFragmentDirections.actionSignUpFragmentToMainFragment())
-        }
+        val application = requireNotNull(this.activity).application
+
+        val database = ArchiveDatabase.getInstance(application)
+
+        val viewModelFactory = SignUpViewModelFactory(database, application)
+
+        viewModel = ViewModelProvider(this, viewModelFactory).get(SignUpViewModel::class.java)
+
+        binding.signUpViewModel = viewModel
+
+        binding.lifecycleOwner = this
+
+        binding.signUpBtn.setOnClickListener(onClickListener)
+        binding.signUpBackBtn.setOnClickListener(onClickListener)
+
+        viewModel.navigateToMain.observe(viewLifecycleOwner, Observer { un: String? ->
+            un?.let{
+                this.findNavController().navigate(SignUpFragmentDirections.actionSignUpFragmentToMainFragment(un))
+            }
+        })
         return binding.root
+    }
+
+    private val onClickListener = View.OnClickListener { view: View ->
+        when (view.id){
+            binding.signUpBtn.id -> {
+                val username = binding.registrationUsernameEditText.text.toString()
+                val firstName = binding.registrationFirstNameEditText.text.toString()
+                val secondName = binding.registrationSecondNameEditText.text.toString()
+                val patronymic = binding.registrationPatronymicEditText.text.toString()
+                val isAdmin = binding.isAdminCheckBox.isChecked
+
+                if ((username != "") and (firstName != "") and (secondName != "") and (patronymic != "")){
+                    viewModel.registration(username, "$secondName $firstName $patronymic", isAdmin)
+                }
+            }
+
+            binding.signUpBackBtn.id -> {
+                view.findNavController().navigate(SignUpFragmentDirections.actionSignUpFragmentToSignInFragment())
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/archive/screens/signUp/SignUpViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.archive.screens.signUp
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.archive.database.ArchiveDatabase
+import com.example.archive.database.enteties.User
+
+class SignUpViewModel(
+        val database: ArchiveDatabase,
+        application: Application): AndroidViewModel(application) {
+
+        private var _navigateToMain = MutableLiveData<String?>()
+
+        val navigateToMain: LiveData<String?>
+            get() = _navigateToMain
+
+            fun registration(un: String, realName: String, perm: Boolean){
+                val newUser = User(un, realName, "sdvs", perm)
+
+                //вставить регистрацию пользователя в бд
+                _navigateToMain.value = newUser.username
+                Log.d("REGISTRATION", "${newUser.username} ${newUser.realName} ${newUser.permissions}")
+            }
+}
